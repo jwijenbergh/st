3121ca775dad2647ec2f0d0037831ceceb5d2a0f
Jeroen Wijenbergh <jeroenwijenbergh@protonmail.com>
2019-08-19 12:35:32 +0200

    Use dracula colors

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Hack:pixelsize=15:antialias=true:autohint=true";
-static int borderpx = 7;
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -97,40 +97,39 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 
-   /* 8 normal colors */
-   [0] = "#212121", /* black   */
-   [1] = "#EC5F67", /* red     */
-   [2] = "#8BD649", /* green   */
-   [3] = "#FFCC00", /* yellow  */
-   [4] = "#89DDFF", /* blue    */
-   [5] = "#82AAFF", /* magenta */
-   [6] = "#80CBC4", /* cyan    */
-   [7] = "#CDD3DE", /* white   */
+  /* 8 normal colors */
+  [0] = "#000000", /* black   */
+  [1] = "#ff5555", /* red     */
+  [2] = "#50fa7b", /* green   */
+  [3] = "#f1fa8c", /* yellow  */
+  [4] = "#bd93f9", /* blue    */
+  [5] = "#ff79c6", /* magenta */
+  [6] = "#8be9fd", /* cyan    */
+  [7] = "#bfbfbf", /* white   */
 
-   /* 8 bright colors */
-   [8]  = "#707880",  /* black   */
-   [9]  = "#EC5F67",  /* red     */
-   [10] = "#8BD649", /* green   */
-   [11] = "#FFCC00", /* yellow  */
-   [12] = "#89DDFF", /* blue    */
-   [13] = "#82AAFF", /* magenta */
-   [14] = "#80CBC4", /* cyan    */
-   [15] = "#FFFFFF", /* white   */
+  /* 8 bright colors */
+  [8]  = "#4d4d4d", /* black   */
+  [9]  = "#ff6e67", /* red     */
+  [10] = "#5af78e", /* green   */
+  [11] = "#f4f99d", /* yellow  */
+  [12] = "#caa9fa", /* blue    */
+  [13] = "#ff92d0", /* magenta */
+  [14] = "#9aedfe", /* cyan    */
+  [15] = "#e6e6e6", /* white   */
 
-   /* special colors */
-   [256] = "#212121", /* background */
-   [257] = "#CDD3DE", /* foreground */
-   [258] = "#CDD3DE",     /* cursor */
+  /* special colors */
+  [256] = "#282a36", /* background */
+  [257] = "#f8f8f2", /* foreground */
 };
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 15;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 15;
-static unsigned int defaultrcs = 15;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
